7fdb2f84d7987a116ee7e600c3900e0634a5400a
gabriel-ribeiro-099 <gabriel.ribeiro.099@ufrn.edu.br>
2023-04-06 22:25:48 -0300

    Atividade 2

---
+++ Atividade2/calculo.hpp
@@ -0,0 +1,9 @@
+#ifndef calculo_hpp
+#define calculo_hpp
+
+float area_quadrado(float);
+float area_retangulo( float , float );
+float area_triangulo(float , float );
+float area_circulo(float );
+
+#endif
